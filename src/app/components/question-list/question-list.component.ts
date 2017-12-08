d3671070bf1ead1a7e3ccf98e73ec14c00824a5e
dedZombie <hemicar92@gmail.com>
2017-12-08 13:37:15 +0100

    DataService

--- src/app/components/question-list/question-list.component.ts
+++ src/app/components/question-list/question-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+import { Question } from '../../models/Question';
 
 @Component({
   selector: 'app-question-list',
@@ -7,26 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionListComponent implements OnInit {
 
-  questions: Object[];
+  questions: Question[];
 
-  constructor() {
-    this.questions = [
-      {
-        text: 'What is your name?',
-        answer: 'My name is Brad'
-      },
-      {
-        text: 'What is your favourite colour?',
-        answer: 'My favourite colour is red'
-      },
-      {
-        text: 'What is your favourite language?',
-        answer: 'My name favourite language is JavaScript'
-      },
-    ];
+  constructor(public dataService: DataService) {
    }
 
   ngOnInit() {
+    this.questions = this.dataService.getQuestions();
   }
 
 }
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Question } from '../models/Question';
+
+@Injectable()
+export class DataService {
+  questions: Question[];
+
+  constructor() {
+    this.questions = [
+      {
+        text: 'What is your name?',
+        answer: 'My name is Brad',
+        hide: true
+      },
+      {
+        text: 'What is your favourite colour?',
+        answer: 'My favourite colour is red',
+        hide: true
+      },
+      {
+        text: 'What is your favourite language?',
+        answer: 'My name favourite language is JavaScript',
+        hide: true
+      }
+    ];
+  }
+
+  getQuestions() {
+    return this.questions;
+  }
+
+}
